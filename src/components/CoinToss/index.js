0d2e37925127d7850d2d9f30b2833462e4e79950
sudhir-kumar-kalivarapu <sudhirkalivarapu@gmail.com>
2023-04-15 10:08:48 +0000

    coin-app

---
+++ src/components/CoinToss/index.js
@@ -0,0 +1,65 @@
+import {Component} from 'react'
+
+import './index.css'
+
+const HEAD_URL = 'https://assets.ccbp.in/frontend/react-js/heads-img.png'
+
+const TAIL_URL = 'https://assets.ccbp.in/frontend/react-js/tails-img.png'
+
+class CoinToss extends Component {
+  state = {
+    tossResultImage: HEAD_URL,
+    headsCount: 0,
+    tailsCount: 0,
+  }
+
+  onTossCoin = () => {
+    const {headsCount, tailsCount} = this.state
+    const toss = Math.floor(Math.random() * 2)
+    let tossImg = ''
+    let latestHeadsCount = headsCount
+    let latestTailsCount = tailsCount
+
+    if (toss === 0) {
+      tossImg = HEAD_URL
+      latestHeadsCount += 1
+    } else {
+      tossImg = TAIL_URL
+      latestTailsCount += 1
+    }
+    this.setState({
+      tossResultImage: tossImg,
+      headsCount: latestHeadsCount,
+      tailsCount: latestTailsCount,
+    })
+  }
+
+  render() {
+    const {tossResultImage, headsCount, tailsCount} = this.state
+    const total = headsCount + tailsCount
+
+    return (
+      <div className="app-container">
+        <div className="coin-toss-container">
+          <h1 className="heading">Coin Toss Game</h1>
+          <p className="description">Heads (or) Tails</p>
+          <img
+            src={tossResultImage}
+            alt="toss result"
+            className="toss-result-img"
+          />
+          <button type="button" className="button" onClick={this.onTossCoin}>
+            Toss Coin
+          </button>
+          <div className="counts-container">
+            <p className="count">{total}</p>
+            <p className="count">{headsCount}</p>
+            <p className="count">{tailsCount}</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CoinToss
